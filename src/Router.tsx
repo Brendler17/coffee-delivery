ef0a4d2306883f6b8f7d749c290e6082d925df98
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-08-30 17:45:00 -0300

    consuming default layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Checkout } from './pages/Checkout';
 import { Delivery } from './pages/Delivery';
+import { DefaultLayout } from './layouts/DefaultLayout';
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/delivery" element={<Delivery />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/delivery" element={<Delivery />} />
+      </Route>
     </Routes>
   );
 }
