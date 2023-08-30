b294124fda3e7a4261331511b657fc2d7b164cf6
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-08-30 15:20:51 -0300

    created light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  title: '#272221',
+  subtitle: '#403937',
+  text: '#574F4D',
+  label: '#8D8686',
+  hover: '#D7D5D5',
+  button: '#E6E5E5',
+  input: '#EDEDED',
+  card: '#F3F2F2',
+  background: '#FAFAFA',
+  white: '#FFFFFF',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-300': '#DBAC2C',
+  'yellow-500': '#C47F17',
+  'purple-100': '#EBE5F9',
+  'purple-300': '#8047F8',
+  'purple-500': '#4B2995',
+};
