bb65b453ed43008bf3c09f3717e1f736e17c03c2
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-08-30 15:40:56 -0300

    consuming router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import { lightTheme } from './styles/themes/light';
 import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
 
 export function App() {
   return (
     <ThemeProvider theme={lightTheme}>
-      <p>Coffee Delivery</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
