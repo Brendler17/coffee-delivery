dcc12f0be1d13fece0b15c3bc7daf50a92f7aed8
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-08-31 21:31:35 -0300

    created homeHeader structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,59 @@
+import {
+  Coffee, Package, ShoppingCart, Timer,
+} from '@phosphor-icons/react';
+import {
+  HomeContainer, HomeHeader, HomeInfo, HomeItens, InfoIten, ListContainer, CoffeeList, CoffeeCard,
+} from './styles';
+import coffeeDeliveryBanner from '../../assets/banner.svg';
+
 export function Home() {
   return (
-    <p>Home</p>
+    <HomeContainer>
+      <HomeHeader>
+        <div>
+          <HomeInfo>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <span>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</span>
+          </HomeInfo>
+          <HomeItens>
+            <InfoIten variant="yellow-dark">
+              <div><ShoppingCart size={16} weight="fill" /></div>
+              <p>Compra simples e segura</p>
+            </InfoIten>
+            <InfoIten variant="gray">
+              <div><Package size={16} weight="fill" /></div>
+              <p>Embalagem mantém o café intacto</p>
+            </InfoIten>
+            <InfoIten variant="yellow">
+              <div><Timer size={16} weight="fill" /></div>
+              <p>Entrega rápida e rastreada</p>
+            </InfoIten>
+            <InfoIten variant="purple">
+              <div><Coffee size={16} weight="fill" /></div>
+              <p>O café chega fresquinho até você</p>
+            </InfoIten>
+          </HomeItens>
+        </div>
+        <img src={coffeeDeliveryBanner} alt="" />
+      </HomeHeader>
+      <ListContainer>
+        <h3>Nossos cafés</h3>
+        <CoffeeList>
+          <CoffeeCard>
+            {/* <img src="" alt="" />
+            <span>Tradicional</span>
+            <h3>Expresso Tradicional</h3>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+            <div>
+              <p>R$9,90</p>
+              <input type="number" />
+              <button type="submit">
+                <ShoppingCart size={16} weight="fill" />
+              </button>
+            </div> */}
+          </CoffeeCard>
+        </CoffeeList>
+      </ListContainer>
+    </HomeContainer>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,84 @@
 import styled from 'styled-components';
 
-export const HomeContainer = styled.div``;
+export const HomeContainer = styled.main`
+  max-width: 70rem;
+  margin: 5rem auto;
+`;
+
+export const HomeHeader = styled.section`
+  display: flex;
+  justify-content: space-between;
+  gap: 1.5rem;
+`;
+
+export const HomeInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  h1{
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 3rem;
+    font-weight: 900;
+    color: ${(props) => props.theme.title}
+  }
+
+  span {
+    font-size: 1.25rem;
+    color: ${(props) => props.theme.subtitle}
+  }
+`;
+
+export const HomeItens = styled.div`
+  display: grid;
+  grid-template-columns: 0.8fr 1fr;
+  gap: 16px;
+  margin-top: 3rem;
+`;
+
+interface InfoItenVariant {
+  variant: 'yellow-dark' | 'yellow' | 'purple' | 'gray';
+}
+
+const InfoItenColorVariants = {
+  'yellow-dark': 'yellow-500',
+  yellow: 'yellow-300',
+  purple: 'purple-300',
+  gray: 'text',
+};
+
+export const InfoIten = styled.div<InfoItenVariant>`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  div {
+    background: ${(props) => props.theme[InfoItenColorVariants[props.variant]]};  
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem;
+    border-radius: 50%;
+  }
+
+  svg {
+    color: ${(props) => props.theme.white};
+  }
+
+  p {
+    color: ${(props) => props.theme.text};
+  }
+`;
+
+export const ListContainer = styled.section`
+  h3{
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 2rem;
+    font-weight: 800;
+    color: ${(props) => props.theme.subtitle};
+    margin-top: 4rem;
+  }
+`;
+
+export const CoffeeList = styled.div``;
+export const CoffeeCard = styled.div``;
