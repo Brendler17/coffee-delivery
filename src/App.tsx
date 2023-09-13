ff8885f90feff10c669b6934bf16ba34dcd52737
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-09-13 10:50:22 -0300

    consuming coffeeContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { lightTheme } from './styles/themes/light';
 import { GlobalStyle } from './styles/global';
 import { Router } from './Router';
+import { CoffeeContextProvider } from './contexts/CoffeeContext';
 
 export function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <BrowserRouter>
-        <Router />
+        <CoffeeContextProvider>
+          <Router />
+        </CoffeeContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
