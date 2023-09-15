7237963ea409163c2e163a1ccfe8e31e8b4fe61c
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-09-15 11:33:26 -0300

    created checkout structure

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,5 +1,131 @@
+import { FormEvent } from 'react';
+import {
+  Bank, CreditCard, CurrencyDollar, MapPinLine, Money, Trash,
+} from '@phosphor-icons/react';
+import {
+  AdressForm, AdressFormHeader, AdressFormInputs, CartContainer, CheckoutFormContainer,
+  CoffeeCartContainer, CoffeeCartForm, CoffeeCartInfo, ConfirmOrderButton,
+  DeliveryContainer, OrderContainer, OrderForm, OrderInfo, OrderTotal,
+  PaymentForm, PaymentFormHeader, PaymentFormInputs, PaymentOption,
+} from './styles';
+
 export function Checkout() {
+  const cartCoffes = [
+    {
+      id: 1,
+      img: '../expresso.svg',
+      name: 'Expresso Tradicional',
+      types: ['tradicional'],
+      description: 'O tradicional café feito com água quente e grãos moídos',
+      price: 9.90,
+    },
+    {
+      id: 2,
+      img: '../americano.svg',
+      name: 'Expresso Americano',
+      types: ['tradicional'],
+      description: 'Expresso diluído, menos intenso que o tradicional',
+      price: 9.90,
+    },
+  ];
+
+  function handleCreateNewOrder(event: FormEvent) {
+    event.preventDefault();
+  }
+
   return (
-    <p>Checkout</p>
+    <CheckoutFormContainer onSubmit={handleCreateNewOrder}>
+      <DeliveryContainer>
+        <h3>Complete seu pedido</h3>
+        <AdressForm>
+          <AdressFormHeader>
+            <MapPinLine size={22} />
+            <div>
+              <p>Endereço de Entrega</p>
+              <span>Informe o endereço onde deseja receber o seu pedido</span>
+            </div>
+          </AdressFormHeader>
+          <AdressFormInputs>
+            <input type="text" placeholder="CEP" />
+            <input type="text" placeholder="Rua" />
+            <div>
+              <input type="text" placeholder="Número" />
+              <input type="text" placeholder="Complemento (Opcional)" />
+            </div>
+            <div>
+              <input type="text" placeholder="Bairro" />
+              <input type="text" placeholder="Cidade" />
+              <input type="text" placeholder="UF" />
+            </div>
+          </AdressFormInputs>
+        </AdressForm>
+        <PaymentForm>
+          <PaymentFormHeader>
+            <CurrencyDollar size={22} />
+            <div>
+              <p>Pagamento</p>
+              <span>O pagamento é feito na entrega. Escolha a forma que deseja pagar</span>
+            </div>
+          </PaymentFormHeader>
+          <PaymentFormInputs>
+            <PaymentOption>
+              <CreditCard size={16} />
+              <span>Cartão de Crédito</span>
+            </PaymentOption>
+            <PaymentOption>
+              <Bank size={16} />
+              <span>Cartão de Débito</span>
+            </PaymentOption>
+            <PaymentOption>
+              <Money size={16} />
+              <span>Dinheiro</span>
+            </PaymentOption>
+          </PaymentFormInputs>
+        </PaymentForm>
+      </DeliveryContainer>
+
+      <OrderContainer>
+        <h3>Cafés selecionados</h3>
+        <OrderForm>
+          <CartContainer>
+            {
+              cartCoffes.map((coffee) => (
+                <CoffeeCartContainer>
+                  <CoffeeCartInfo>
+                    <img src={coffee.img} alt="" />
+                    <div>
+                      <p>{coffee.name}</p>
+                      <CoffeeCartForm>
+                        <input type="number" />
+                        <button type="button">
+                          <Trash size={16} />
+                          Remover
+                        </button>
+                      </CoffeeCartForm>
+                    </div>
+                  </CoffeeCartInfo>
+                  <span>{`R$${coffee.price}0`.replace('.', ',')}</span>
+                </CoffeeCartContainer>
+              ))
+            }
+          </CartContainer>
+          <OrderInfo>
+            <div>
+              <span>Total de Itens</span>
+              <span>R$ 20,90</span>
+            </div>
+            <div>
+              <span>Entrega</span>
+              <span>R$ 3,50</span>
+            </div>
+            <OrderTotal>
+              <span>Total</span>
+              <span>R$ 24,40</span>
+            </OrderTotal>
+          </OrderInfo>
+          <ConfirmOrderButton type="submit">Confirmar Pedido</ConfirmOrderButton>
+        </OrderForm>
+      </OrderContainer>
+    </CheckoutFormContainer>
   );
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,291 @@
 import styled from 'styled-components';
 
-export const CheckoutContainer = styled.div``;
+export const CheckoutFormContainer = styled.form`
+  max-width: 70rem;
+  margin: calc(5rem + 110px) auto 0;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  h3 {
+    font-family: 'Baloo 2', cursive;
+    font-size: 1.125rem;
+    font-weight: 700;
+    color: ${(props) => props.theme.subtitle};
+    margin-bottom: 1rem;
+  }
+`;
+
+export const DeliveryContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
+
+export const AdressForm = styled.div`
+  background: ${(props) => props.theme.card};
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  border-radius: 6px;
+`;
+
+export const AdressFormHeader = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  svg {
+    color: ${(props) => props.theme['yellow-500']}
+  }
+
+  p {
+    color: ${(props) => props.theme.subtitle};
+  }
+
+  span {
+    color: ${(props) => props.theme.text};
+    font-size: 0.875rem;
+  }
+`;
+
+export const AdressFormInputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  input {
+    background: ${(props) => props.theme.input};
+    border: 1px solid ${(props) => props.theme.button};
+    border-radius: 4px;
+    padding: 0.75rem;
+    color: ${(props) => props.theme.label};
+    font-size: 0.875rem;
+
+    &:nth-child(1){
+      width: 200px;
+    }
+  }
+
+  div {
+    display: flex;
+    gap: 1rem;
+  }
+
+  div:nth-child(3) {
+    input:nth-child(2) {
+      flex: 1;
+    }
+  }
+
+  div:nth-child(4) {
+    input:nth-child(3) {
+      width: 60px;
+    }
+  }
+`;
+
+export const PaymentForm = styled.div`
+  background: ${(props) => props.theme.card};
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  border-radius: 6px;
+`;
+
+export const PaymentFormHeader = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  svg {
+    color: ${(props) => props.theme['purple-300']};
+  }
+
+  p {
+    color: ${(props) => props.theme.subtitle};
+  }
+
+  span {
+    color: ${(props) => props.theme.text};
+    font-size: 0.875rem;
+  }
+`;
+
+export const PaymentFormInputs = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.75rem;
+`;
+
+export const PaymentOption = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  background: ${(props) => props.theme.button};
+  border-radius: 6px;
+  gap: 0.75rem;
+  cursor: pointer;
+
+  svg {
+    color: ${(props) => props.theme['purple-300']};
+  }
+
+  span {
+    color: ${(props) => props.theme.text};
+    font-size: 0.75rem;
+    line-height: 1.6;
+    text-transform: uppercase;
+  }
+`;
+
+export const OrderContainer = styled.aside`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
+
+export const OrderForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+  border-radius: 6px 44px 6px 44px;
+  background: ${(props) => props.theme.card};
+  gap: 1.5rem;
+`;
+
+export const CartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 350px;
+
+  div + div {
+    padding-top: 1.5rem;
+  }
+`;
+
+export const CoffeeCartContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  border-bottom: 1px solid ${(props) => props.theme.button};
+  padding-bottom: 1.5rem;
+
+  > span {
+    color: ${(props) => props.theme.text};
+    font-weight: 700;
+  }
+`;
+
+export const CoffeeCartInfo = styled.div`
+  display: flex;
+  gap: 1.25rem;
+
+  img {
+    width: 64px;
+    height: 64px;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  p {
+    color: ${(props) => props.theme.subtitle};
+  }
+`;
+
+export const CoffeeCartForm = styled.div`
+  display: flex;
+  gap: 8px;
+
+  input {
+    width: 4.5rem;
+    border: 0;
+    border-radius: 6px;
+    background: ${(props) => props.theme.button};
+    padding: 0.5rem;
+    color: ${(props) => props.theme.text};
+    text-align: center;
+    line-height: 0;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    padding: 0 0.5rem;
+    gap: 4px;
+    border: 0;
+    border-radius: 6px;
+    background: ${(props) => props.theme.button};
+    
+    font-size: 0.75rem;
+    color: ${(props) => props.theme.text};
+    line-height: 1.6;
+    text-transform: uppercase;
+    cursor: pointer;
+
+    svg {
+      color: ${(props) => props.theme['purple-300']};
+    }
+  }
+`;
+
+export const OrderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  
+  div {
+    display: flex;
+    justify-content: space-between;
+
+    &:nth-child(1){
+      color: ${(props) => props.theme.text};
+
+     span {
+      &:first-child{
+        font-size: 0.875rem;
+      }
+     }
+    }
+
+    &:nth-child(2){
+      color: ${(props) => props.theme.text};
+
+     span {
+      &:first-child{
+        font-size: 0.875rem;
+      }
+     }
+    }
+  }
+
+  button {
+    padding: 0.75rem 0.5rem;
+  }
+`;
+
+export const OrderTotal = styled.div`
+  font-size: 1.25rem;
+  font-weight: 700;
+  color: ${(props) => props.theme.subtitle};
+`;
+
+export const ConfirmOrderButton = styled.button`
+  padding: 0.75rem 0.5rem;
+  background: ${(props) => props.theme['yellow-300']};
+  border: 0;
+  border-radius: 6px;
+  cursor: pointer;
+
+  color: ${(props) => props.theme.white};
+  font-size: 0.875rem;
+  font-weight: 700;
+  line-height: 1.6;
+  text-transform: uppercase;
+`;
