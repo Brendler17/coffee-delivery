91a8abf417c500f0dfaf4a487fe709b63abcc261
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2023-09-15 11:33:49 -0300

    created delivery structure

--- src/pages/Delivery/index.tsx
+++ src/pages/Delivery/index.tsx
@@ -1,5 +1,45 @@
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react';
+import {
+  Adress, DeliveryContainer, DeliveryContent, DeliveryHeader, DeliveryInfo, InfoIten,
+} from './styles';
+import deliveryBanner from '../../assets/delivery.svg';
+
 export function Delivery() {
   return (
-    <p>Delivery</p>
+    <DeliveryContainer>
+      <DeliveryHeader>
+        <p>Uhu! Pedido confirmado</p>
+        <span>Agora é só aguardar que logo o café chegará até você</span>
+      </DeliveryHeader>
+      <DeliveryContent>
+        <DeliveryInfo>
+          <InfoIten variant="purple">
+            <div><MapPin size={16} weight="fill" /></div>
+            <div>
+              <Adress>
+                <span>Entrega em</span>
+                <p>Rua Dr. Armando Fagundes, 575</p>
+              </Adress>
+              <span>Areal - Pelotas, RS</span>
+            </div>
+          </InfoIten>
+          <InfoIten variant="yellow">
+            <div><Timer size={16} weight="fill" /></div>
+            <div>
+              <span>Previsão de Entrega</span>
+              <p>20min - 30min</p>
+            </div>
+          </InfoIten>
+          <InfoIten variant="yellow-dark">
+            <div><CurrencyDollar size={16} /></div>
+            <div>
+              <span>Pagamento na Entrega</span>
+              <p>Cartão de Crédito</p>
+            </div>
+          </InfoIten>
+        </DeliveryInfo>
+        <img src={deliveryBanner} alt="" />
+      </DeliveryContent>
+    </DeliveryContainer>
   );
 }
--- src/pages/Delivery/styles.ts
+++ src/pages/Delivery/styles.ts
@@ -1,3 +1,89 @@
 import styled from 'styled-components';
 
-export const DeliveryContainer = styled.div``;
+export const DeliveryContainer = styled.div`
+  max-width: 70rem;
+  margin: calc(5rem + 110px) auto 0;
+`;
+
+export const DeliveryHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+
+  p {
+    font-family: 'Baloo 2', cursive;
+    font-size: 2rem;
+    font-weight: 800;
+    color: ${(props) => props.theme['yellow-500']};
+  }
+
+  span {
+    font-size: 1.25rem;
+    color: ${(props) => props.theme.subtitle};
+  }
+`;
+export const DeliveryContent = styled.main`
+  margin-top: 2.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const DeliveryInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 2.5rem;
+  min-width: 500px;
+
+  border: 1px solid;
+  border-radius: 6px 36px 6px 36px;
+  border-top-color: ${(props) => props.theme['yellow-300']};
+  border-left-color: ${(props) => props.theme['yellow-300']};
+  border-right-color: ${(props) => props.theme['purple-300']};
+  border-bottom-color: ${(props) => props.theme['purple-300']};
+`;
+
+interface InfoItenVariant {
+  variant: 'yellow-dark' | 'yellow' | 'purple';
+}
+
+const InfoItenColorVariants = {
+  'yellow-dark': 'yellow-500',
+  yellow: 'yellow-300',
+  purple: 'purple-300',
+};
+
+export const InfoIten = styled.div<InfoItenVariant>`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  > div:first-child {
+    background: ${(props) => props.theme[InfoItenColorVariants[props.variant]]};  
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem;
+    border-radius: 50%;
+  }
+
+  svg {
+    color: ${(props) => props.theme.white};
+    line-height: 0;
+  }
+
+  span, p {
+    color: ${(props) => props.theme.text};
+  }
+
+  p {
+    font-weight: 700;
+  }
+
+`;
+
+export const Adress = styled.div`
+  display: flex;
+  gap: 4px;
+`;
