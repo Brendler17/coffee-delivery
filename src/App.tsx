77aa1644a91ff91a716e0a7ba48fa9d7d248de65
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2024-09-10 20:42:14 -0300

    app updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { lightTheme } from './styles/themes/light';
 import { GlobalStyle } from './styles/global';
 import { Router } from './Router';
-import { CoffeeContextProvider } from './contexts/CoffeeContext';
 
 export function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <BrowserRouter>
-        <CoffeeContextProvider>
-          <Router />
-        </CoffeeContextProvider>
+        <Router />
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
