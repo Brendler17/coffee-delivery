d8ccdf5de855ab11434c0099bad3bf0be5d4a0d4
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2024-09-11 23:55:33 -0300

    created quantity manipulation

--- src/components/CoffeeCard/index.tsx
+++ src/components/CoffeeCard/index.tsx
@@ -1,8 +1,10 @@
-import { ShoppingCart } from '@phosphor-icons/react';
+import { Minus, Plus, ShoppingCart } from '@phosphor-icons/react';
+import { useContext, useState } from 'react';
 import {
   Action,
   Card, Description, Form, Img, Price, Title, Types,
 } from './styles';
+import { CartContext } from '../../contexts/CartContext';
 
 type Props = {
   coffee: {
@@ -16,6 +18,24 @@ type Props = {
 };
 
 export function CoffeeCard({ coffee }: Props) {
+  const [quantity, setQuantity] = useState(1);
+
+  const { addNewCoffeeToCart } = useContext(CartContext);
+
+  function handleAddItem() {
+    addNewCoffeeToCart({ id: coffee.id, quantity });
+  }
+
+  function incrementQuantity() {
+    setQuantity((state) => state + 1);
+  }
+
+  function decrementQuantity() {
+    if (quantity > 1) {
+      setQuantity((state) => state - 1);
+    }
+  }
+
   return (
     <Card>
 
@@ -39,12 +59,16 @@ export function CoffeeCard({ coffee }: Props) {
         </Price>
 
         <Action>
-          <input
-            id="quantity"
-            type="number"
-            min={1}
-          />
-          <button type="button">
+          <div>
+            <button type="button" onClick={decrementQuantity}>
+              <Minus />
+            </button>
+            <span>{quantity}</span>
+            <button type="button" onClick={incrementQuantity}>
+              <Plus />
+            </button>
+          </div>
+          <button type="button" onClick={handleAddItem}>
             <ShoppingCart size={22} weight="fill" color="white" />
           </button>
         </Action>
